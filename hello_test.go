148fbbe27f5eecc93e2b9b7d6d874944856928a1
Abdullah Hassen <am.hassen977@gmail.com>
2024-03-03 14:35:30 +0200

    refactor #2 with added language support

--- hello_test.go
+++ hello_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	t.Run(("saying hello to people"), func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
@@ -13,13 +13,30 @@ func TestHello(t *testing.T) {
 	})
 
 	t.Run(("say 'Hello, World' when an empty string is supplied"), func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 
 	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Afrikaans", func(t *testing.T) {
+		got := Hello("Elodie", "Afrikaans")
+		want := "More, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
